e2c7a897467e1bdef07d2602c659c0f230e37d3a
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-09-10 21:31:51 +0100

    feat(card): JS templating players correctly

--- src/js/playerstats.js
+++ src/js/playerstats.js
@@ -8,15 +8,58 @@ class PlayerStats {
     stats.then(response => {
       this.init(response);
     })
+
+    this.config = {
+      template: '.js-player-template',
+      player: '.js-player',
+      card: '.js-card'
+    }
   }
 
   init(response) {
     this.statsData = response;
+    this.getTemplate();
+    // this.setTemplate();
     this.render();
   }
 
+  getTemplate() {
+    this.template = document.querySelector(this.config.template).innerHTML;
+  }
+
+  // setTemplate() {
+  //   const card = document.querySelector(this.config.card);
+  //   card.insertAdjacentHTML('afterbegin', this.template);
+  // }
+
   render() {
-    console.log('render', this.statsData);
+    // const data = this.statsData
+    // for (let player in data) {
+    //   console.log(player);
+    //   console.log(data[player]);
+    // }
+
+    var listHtml = '';
+
+    const data = this.statsData
+
+    for (var player in data) {
+      console.log(data[player]);
+      listHtml += this.template
+        .replace(/{name}/g, data[player]['name'])
+        .replace(/{position}/g, data[player]['position'])
+        .replace(/{appearances}/g, data[player].stats['appearances'])
+        .replace(/{goals}/g, data[player].stats['goals'])
+        .replace(/{assists}/g, data[player].stats['goal_assist'] || 0)
+        .replace(/{gpm}/g, Math.floor(data[player].stats['goals'] / data[player].stats['appearances']))
+        .replace(/{ppm}/g, Math.floor(data[player].stats['backward_pass'] + data[player].stats['fwd_pass'] / data[player].stats['mins_played']))
+        .replace(/{id}/g, data[player]['id'])
+        .replace(/{teamSlug}/g, data[player]['teamSlug'])
+    }
+
+    const card = document.querySelector(this.config.card);
+    card.insertAdjacentHTML('afterbegin', listHtml);
+
   }
 }
 
